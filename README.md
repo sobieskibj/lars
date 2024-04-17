c76bd3744e7e050b983d6b854f3579fba501e017
sobieskibj <sobieski.bartlomiej.jan@gmail.com>
2024-04-17 12:12:27 +0200

    Add to do

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ The LARS algorithm was introduced in the work of [Efron et al. (2004)](https://a
 # Materials
 
 - [Introduction to LARS](https://b-thi.github.io/pdfs/LARS.pdf)
+
+# To do
+
+- porownac wyniki roznych implementacji lasso z wlasna implementacja lars na kilku zbiorach danych
+    - wielkosc wspolczynnikow i liczba niezerowych
+    - czas dzialania
+- sprawdzic sytuacje danych zaleznych
--- src/model/lars.py
+++ src/model/lars.py
@@ -12,6 +12,33 @@ class LARS(nn.Module):
     def fit(self, dataset):
         X, y = dataset.get_data()
 
-        import pdb; pdb.set_trace()
+        mu_hat = torch.zeros_like(y)
+        s = torch.zeros_like(X[0, None].T)
 
+        for k in range(self.p):
+
+            c_hat = X.T @ (y - mu_hat)
+
+            C_hat = c_hat.abs().max()
+
+            # probably not needed as we should start with empty A
+            A = c_hat.abs() == C_hat
+            
+            s[A] = c_hat[A].sign()
+
+            X_A = X @ s
+            G_A = X_A.T @ X_A
+            G_A_inv = G_A.inverse()
+            I_A = torch.ones_like(A).int()
+            import pdb; pdb.set_trace()
+            A_A = G_A_inv.sum(dim = 0).sum(dim = 0) ** (-1/2)
+            w_A = A_A * G_A_inv.sum(dim = 1).view(-1, 1)
+            u_A = X_A @ w_A
+            import pdb; pdb.set_trace()
+            a = X.T @ u_A
+            gammas = ...
+            j = gammas.argmin()
+            gamma = gammas[j]
+            mu_hat += gamma * u_A
+            A[j] = True
 
